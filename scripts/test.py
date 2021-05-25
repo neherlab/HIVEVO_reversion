1bc62ce8b54832b86de48c1ee9d2cb853e103712
Valentin <valentin.druelle@unibas.ch>
2021-05-25 11:42:25 +0200

    random test for typing

--- scripts/test.py
+++ scripts/test.py
@@ -1,13 +1,14 @@
-# Adds link to the scripts folder
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
 
-def sum(a: np.array, b: float) -> float :
+
+def sum(a: np.ndarray, b: float) -> float:
     return np.sum(a) + b
 
+
 if __name__ == "__main__":
-    a = np.array([1,2,3])
+    a = np.array([1, 2, 3])
     b = np.array([1])
     print(sum(a, b))
