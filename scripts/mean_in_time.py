4c32cde170f5b20066ea6e0ee838c092b65d1dfc
Valentin <valentin.druelle@unibas.ch>
2021-05-31 12:01:37 +0200

    Mean in time figure

--- scripts/mean_in_time.py
+++ scripts/mean_in_time.py
@@ -1,27 +1,31 @@
 import matplotlib.pyplot as plt
 import filenames
 import trajectory
-import bootstrap
 
 if __name__ == "__main__":
+    # Parameters
+    trajectory_file = "data/Trajectory_list_any.json"
+    mean_in_time_file = "data/bootstrap_mean_dict_any.json"
     fontsize = 16
     fill_alpha = 0.15
     grid_alpha = 0.5
     ticksize = 14
-
-    trajectories = trajectory.load_trajectory_dict("trajectory_dict")
-    times = trajectory.create_time_bins()
-    times = 0.5 * (times[:-1] + times[1:]) / 365
-    bootstrap_dict = load_dict()
-    trajectories_scheme = trajectory.get_trajectories_offset(trajectories["all"]["rev"], [0.4, 0.6])
-
     colors = ["C0", "C1", "C2", "C4"]
+
+    # Data loading
+    trajectories = trajectory.load_trajectory_list(trajectory_file)
+    times = trajectory.create_time_bins(400)
+    times = 0.5 * (times[:-1] + times[1:]) / 365  # In years
+    bootstrap_dict = trajectory.load_mean_in_time_dict(mean_in_time_file)
+
+    # Selecting reversion trajectories in [0.4, 0.6] for left pannel
     freq_ranges = [[0.2, 0.4], [0.4, 0.6], [0.6, 0.8]]
+    trajectories_scheme = [traj for traj in trajectories if traj.reversion]
+    trajectories_scheme = trajectory.offset_trajectories(trajectories_scheme, [0.4, 0.6])
 
     fig, axs = plt.subplots(ncols=2, nrows=1, figsize=(14, 7), sharey=True)
 
     # Plot left
-
     for traj in trajectories_scheme:
         axs[0].plot(traj.t / 365, traj.frequencies, "k-", alpha=0.1, linewidth=1)
 
@@ -58,12 +62,11 @@ if __name__ == "__main__":
     line5, = axs[1].plot([0], [0], "-", color=colors[2])
 
     axs[1].set_xlabel("Time [years]", fontsize=fontsize)
-    # axs[1].set_ylabel("Frequency", fontsize=fontsize)
     axs[1].set_ylim([-0.03, 1.03])
     axs[1].grid(grid_alpha)
     axs[1].tick_params(axis="x", labelsize=ticksize)
     axs[1].legend([line3, line4, line5, line1, line2],
                   ["[0.2, 0.4]", "[0.4, 0.6]", "[0.6, 0.8]", "reversion", "non-reversion"],
                   fontsize=fontsize, ncol=2, loc="lower right")
-
+    plt.tight_layout()
     plt.show()
