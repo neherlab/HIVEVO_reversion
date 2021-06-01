b5269ac6a041b660aeaaaa088f6398d1146b2b1b
Valentin <valentin.druelle@unibas.ch>
2021-06-01 10:55:50 +0200

    Sweep position analysis

--- scripts/test.py
+++ scripts/test.py
@@ -1,14 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import os
+import filenames
+import tools
+from hivevo.patients import Patient
 
 
-def sum(a: np.ndarray, b: float) -> float:
-    return np.sum(a) + b
+def get_sweeps(patient, aft, region):
+    "Returns the position and nucleotide of sweeps hapening in the aft."
+    # Masking low depth
+    depth = tools.depth_mask(patient, region)
+    aft.mask = np.logical_or(aft.mask, ~depth)
+
+    # Set all initial idx to 0
+    initial_idx = patient.get_initial_indices(region)
+    aft[np.arange(aft.shape[0])[:, np.newaxis, np.newaxis], initial_idx, np.arange(aft.shape[-1])] = 0
+
+    # Find the nucleotide and position of sweeps
+    sweeps = aft > 0.5
+    sweeps = np.sum(sweeps, axis=0, dtype=bool)
+    sweeps_idxs = np.where(sweeps)
+    p = sweeps_idxs[1].argsort()
+    positions = sweeps_idxs[1][p]
+    nucleotides = sweeps_idxs[0][p]
+
+    return nucleotides, positions
+
+
+    # Map that to the synonymous mask
+    # syn_mask = patient.get_syn_mutations(region)
+    # synonymous = syn_mask[nucleotides, positions]
+    # breakpoint()
 
 
 if __name__ == "__main__":
-    a = np.array([1, 2, 3])
-    b = np.array([1])
-    print(sum(a, b))
+    region = "env"
+    fontsize = 16
+    patient = Patient.load("p1")
+    aft = patient.get_allele_frequency_trajectories(region)
+    nucleotides, positions = get_sweeps(patient, aft, region)
