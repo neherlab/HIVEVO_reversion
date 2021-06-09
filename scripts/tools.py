7de21e5ff024d139263713b7f07c860ae1ce8dc3
Valentin <valentin.druelle@unibas.ch>
2021-06-09 11:50:59 +0200

    Documentation

--- scripts/tools.py
+++ scripts/tools.py
@@ -7,16 +7,25 @@ from hivevo.patients import Patient
 
 
 def mutation_positions_mask(patient, region, aft, eps=0.01):
-    """
-    Return a 1D boolean matrix where True are positions where new mutations are seen at some time with more
+    """Returns a 1D boolean matrix where True are positions where new mutations are seen at some time with more
     than eps frequency.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+        aft (np.array): Allele frequency trajectories (3D).
+        eps (float): Threshold for mutation detection.
+
+    Returns:
+        (np.array): Mask for mutation positions (1D).
+
     """
     initial_idx = patient.get_initial_indices(region)
     aft_initial = aft[np.arange(aft.shape[0])[:, np.newaxis, np.newaxis],
                       initial_idx, np.arange(aft.shape[-1])]
     aft_initial = np.ma.squeeze(aft_initial)
-    aft_initial = np.ma.sum(aft_initial < 1 - eps, axis=0, dtype=bool)
-    return aft_initial
+    mask = np.ma.sum(aft_initial < 1 - eps, axis=0, dtype=bool)
+    return mask
 
 
 # def get_fixation_positions(patient, region, aft, eps=0.01, timepoint="any"):
@@ -36,9 +45,19 @@ def mutation_positions_mask(patient, region, aft, eps=0.01):
 
 
 def reference_filter_mask(patient, region, aft, ref, gap_threshold=0.1):
-    """
-    Returns a 1D boolean vector where False are the positions (in aft.shape[-1]) that are unmapped to
-    reference or too often gapped.
+    """Returns a 1D boolean vector where False are the genome positions that are unmapped to reference or too
+    often gapped.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+        aft (np.array): Allele frequency trajectories (3D).
+        ref (HIVreference): HIVreference object from hivevo package.
+        gap_threshold (float): Max frequency of gaps at a given position.
+
+    Returns:
+        (np.array): Mask for reference / ungapped genome positions (1D).
+
     """
     map_to_ref = patient.map_to_external_reference(region)
     ungapped_genomewide = ref.get_ungapped(gap_threshold)
@@ -54,9 +73,15 @@ def reference_filter_mask(patient, region, aft, ref, gap_threshold=0.1):
 
 
 def depth_mask(patient, region):
-    """
-    Returns boolean matrix (same shape as aft) where True are samples where the depth was labeled "ok" in the
-    tsv files.
+    """Returns boolean matrix where True are samples where the depth was labeled "ok" in the tsv files.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+
+    Returns:
+        (np.array): Mask for non problematic depth (same shape as aft).
+
     """
     fragments, fragment_names = get_fragment_per_site(patient, region)
     fragment_depths = [get_fragment_depth(patient, frag) for frag in fragment_names]
@@ -67,7 +92,18 @@ def depth_mask(patient, region):
 
 
 def associate_depth(fragments, fragment_depths, fragment_names):
-    "Associate a bolean array (true where coverage is ok) to each positions of the region."
+    """Associate a bolean array (true where coverage is ok) to each positions of the region.
+
+    Args:
+        fragments (list(list(string))): List containing a list of fragments of each genome positions.
+        fragment_depths (list(list(string))): List containing a list of depth label for each fragment at all
+                                              timepoints.
+        fragment_names (list(string)): The fragment names in the region.
+
+    Returns:
+        (np.array): Bool 2D matrix where True are the positions/time where depth is ok.
+
+    """
     bool_frag_depths = np.array(fragment_depths) == "ok"
     depths = []
     for ii in range(len(fragments)):
@@ -84,8 +120,16 @@ def associate_depth(fragments, fragment_depths, fragment_names):
 
 
 def get_fragment_per_site(patient, region):
-    """
-    Returns a list of fragment associated to each position in the region.
+    """Returns a list of fragment associated to each position in the region and a list of the fragment names.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+
+    Returns:
+        (list(list(string))): List containing a list of depth label for each fragment at all timepoints.
+        (list(string)): List containing the fragment names in the region.
+
     """
     fragment_list = [[]] * len(patient._region_to_indices(region))
     frag = patient._annotation_to_fragment_indices(region)
@@ -100,14 +144,31 @@ def get_fragment_per_site(patient, region):
 
 
 def get_fragment_depth(patient, fragment):
-    "Returns the depth of the fragment for each time point."
+    """Returns the depth of the fragment for each time point.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+
+    Returns:
+        list(string): List of the depth for each timepoint in the given fragment.
+
+    """
     return [s[fragment] for s in patient.samples]
 
 
 def reversion_map(patient, region, aft, ref):
-    """
-    Returns a 2D boolean matrix (nucleotide*patient_sequence_length) where True are the positions that
-    correspond to the reference nucleotide.
+    """Returns a 2D boolean matrix where True are the positions correspond to the reference nucleotide.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+        aft (np.array): Allele frequency trajectories (3D).
+        ref (HIVreference): HIVreference object from hivevo package.
+
+    Returns:
+        (np.array): Mask where True are the reference nucleotide (2D: aft.shape[1]*aft.shape[2]).
+
     """
     reversion_map = np.zeros((aft.shape[1], aft.shape[2]), dtype="bool")
     map_to_ref = patient.map_to_external_reference(region)
@@ -118,9 +179,18 @@ def reversion_map(patient, region, aft, ref):
 
 
 def get_fitness_cost(patient, region, aft, subtype="any"):
-    """
-    Returns a 1D vector (patient_sequence_length) with the fitness coefficient for each sites. Sites missing
-    from the consensus sequence or without fitness_cost associated are nans.
+    """Returns a 1D vector of fitness coefficient for each sites. Sites missing from the consensus sequence or
+       without fitness_cost associated are nans.
+
+    Args:
+        patient (Patient): The patient to analyse.
+        region (string): Region to analyse ['env', 'pol', 'gag'].
+        aft (np.array): Allele frequency trajectories (3D).
+        subtype (string): either 'any' or 'B'.
+
+    Returns:
+        np.array: Fitness coefficient of each genome position.
+
     """
     if subtype not in ["any", "B"]:  # Fitness was computed only for global consensus and subtype B consensus
         tmp = np.empty(aft.shape[-1])
