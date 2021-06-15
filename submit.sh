6e377de7c7fafab8dbdf3ced7b0df957b4f56c79
Valentin <valentin.druelle@unibas.ch>
2021-06-15 17:09:13 +0200

    logfile

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --output=log/%j.out                 # where to store the output ( %j is the JOBID )
-#SBATCH --error=log/%j.err                  # where to store error messages
+#SBATCH --output=/scicore/home/neher/druell0000/PhD/HIVEVO_reversion/log/%j.out                 # where to store the output ( %j is the JOBID )
+#SBATCH --error=/scicore/home/neher/druell0000/PhD/HIVEVO_reversion/log/%j.err                  # where to store error messages
 
 # activate conda environment
 source /scicore/home/neher/druell0000/miniconda3/condabin/conda
