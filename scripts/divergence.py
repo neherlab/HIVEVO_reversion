0bc552aa47dd5a3645e125afece29a880141e8e0
Valentin <valentin.druelle@unibas.ch>
2021-06-16 17:25:20 +0200

    Divergence computation script

---
+++ scripts/divergence.py
@@ -0,0 +1,59 @@
+import filenames
+import tools
+import numpy as np
+
+from hivevo.HIVreference import HIVreference
+from hivevo.patients import Patient
+
+
+def divergence_matrix(aft, raw_mask, opposite_mask):
+    """
+    Returns the divergence matrix over time (3D). raw_mask (resp opposite_mask) are 1D boolean vectors where
+    True positions contribute their frequency (resp 1-frequency) at each time point.
+    """
+    div = np.zeros_like(aft)
+    for ii in range(aft.shape[0]):
+        div[ii, :, :] = raw_mask * aft[ii, :, :] + opposite_mask * (1 - aft[ii, :, :])
+
+    return div
+
+
+def divergence_in_time(patient, region, aft, div_ref):
+    """
+    Returns the divergence in time matrix (2D) for each genome position at each time point.
+    div_ref specifies the reference to which the divergence needs to be computed.
+    """
+    assert div_ref in ["founder", "any", "B", "C"]  # founder is founder sequence, any is global consensus
+
+    initial_idx = patient.get_initial_indices(region)
+    if div_ref == "founder":
+        aft_initial = aft[np.arange(aft.shape[0])[:, np.newaxis, np.newaxis],
+                          initial_idx, np.arange(aft.shape[-1])]
+        aft_initial = np.squeeze(aft_initial)
+        raw_mask = np.zeros(aft.shape[-1])
+        opposite_mask = aft_initial[0]
+        div_3D = divergence_matrix(aft, raw_mask, opposite_mask)
+    else:
+        ref = HIVreference(subtype=div_ref)
+        raw_mask = tools.non_reference_mask(patient, region, aft, ref)
+        opposite_mask = tools.reference_mask(patient, region, aft, ref)
+        div_3D = divergence_matrix(aft, raw_mask, opposite_mask)
+
+    div = div_3D[np.arange(aft.shape[0])[:, np.newaxis, np.newaxis], initial_idx, np.arange(aft.shape[-1])]
+    return np.squeeze(div)
+
+
+def mean_divergence_in_time(patient, region, aft, div_ref):
+    """
+    Returns the average over all positions of divergence_in_time(patient, region, aft, div_ref)
+    """
+    return np.mean(divergence_in_time(patient, region, aft, div_ref), axis=1)
+
+
+if __name__ == '__main__':
+    region = "env"
+    patient = Patient.load("p2")
+    aft = patient.get_allele_frequency_trajectories(region)
+    div = mean_divergence_in_time(patient, region, aft, "founder")
+    div = mean_divergence_in_time(patient, region, aft, "any")
+    div = mean_divergence_in_time(patient, region, aft, "B")
