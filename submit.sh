50a8a4f141bd9babd361ea8efb73d8939b863b4b
Valentin <valentin.druelle@unibas.ch>
2021-06-16 09:48:35 +0200

    changed logfile names

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --output=log/%j.out                 # where to store the output ( %j is the JOBID )
-#SBATCH --error=log/%j.err                  # where to store error messages
+#SBATCH --output=log/$SLURM_JOB_ID.out                 # where to store the output ( %j is the JOBID )
+#SBATCH --error=log/%SLURM_JOB_ID.err                  # where to store error messages
 
 #Run .bashrc to initialize conda
 source $HOME/.bashrc
