336d59ab28b2c715ab3cadf09b10889d0e35e6ec
Valentin <valentin.druelle@unibas.ch>
2021-06-25 16:44:39 +0200

    WH rates to the mutation rate figure

--- scripts/mutation_rates.py
+++ scripts/mutation_rates.py
@@ -1,16 +1,18 @@
 import json
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from Bio import AlignIO, Phylo
 from distance_in_time import get_reference_sequence, get_mean_distance_in_time, get_root_to_tip_distance
+import divergence
 
 
-def compute_mutation_rates(nb_sequences = [1000, 500, 250, 125, 60, 30]):
+def compute_mutation_rates(nb_sequences=[1000, 500, 250, 125, 60, 30]):
     """
     Returns the mutation rates from the distance to root, distance to subtype consensus and root to tip
     distance.
     """
-    # The files used to compute the rates
+    # BH rate files
     reference_file = {}
     reference_file["root"] = "data/BH/intermediate_files/pol_1000_nt_muts.json"
     reference_file["B"] = "data/BH/alignments/to_HXB2/pol_1000_B_consensus.fasta"
@@ -21,6 +23,10 @@ def compute_mutation_rates(nb_sequences = [1000, 500, 250, 125, 60, 30]):
     for nb in nb_sequences:
         tree_file[str(nb)] = "data/BH/intermediate_files/timetree_pol_" + str(nb) + ".nwk"
         branch_length_file[str(nb)] = "data/BH/intermediate_files/branch_lengths_pol_" + str(nb) + ".json"
+
+    # WH rates
+    WH_file = "data/WH/avg_rate_dict.json"
+    WH_rate_dict = divergence.load_avg_rate_dict(WH_file)
 
     # Rates from hamming distance
     rates = {"root": {}, "subtypes": {}}
@@ -47,6 +53,9 @@ def compute_mutation_rates(nb_sequences = [1000, 500, 250, 125, 60, 30]):
         fit = np.polyfit(dates[key], lengths[key], deg=1)
         rates["rtt"][key] = fit[0]
 
+    # WH rates from divergence
+    rates["WH"] = WH_rate_dict["pol"]
+
     return rates
 
 
@@ -55,29 +64,38 @@ def plot_mutation_rates():
     Plot for the mutation rates.
     """
     rates = compute_mutation_rates()
-    labels = ["Hamming root", "Hamming subtype", "RTT", "WH"]
+    labels = ["Hamming root", "Hamming subtype", "RTT", "WH_global", "WH_subtypes", "WH_founder"]
 
     fontsize = 16
     markersize = 16
-    colors = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7"]
+    colors = ["C0", "C1", "C2", "C3", "C4", "C5"]
+    cmap = matplotlib.cm.get_cmap('viridis')
+    cmap_colors = [cmap(x) for x in np.linspace(0,1, len(rates["rtt"].keys()))]
 
-    plt.figure()
+    plt.figure(figsize=(10,7))
     for ii, key in enumerate(["root", "subtypes"]):
         if ii:
-            plt.plot(ii, rates[key]["B"]*1e4, '.', color="C0", markersize=markersize, label="B")
-            plt.plot(ii, rates[key]["C"]*1e4, '.', color="C1", markersize=markersize, label="C")
+            plt.plot(ii, rates[key]["B"] * 1e4, '.', color="C0", markersize=markersize, label="B")
+            plt.plot(ii, rates[key]["C"] * 1e4, '.', color="C1", markersize=markersize, label="C")
         else:
-            plt.plot(ii, rates[key]["B"]*1e4, '.', color="C0", markersize=markersize)
-            plt.plot(ii, rates[key]["C"]*1e4, '.', color="C1", markersize=markersize)
+            plt.plot(ii, rates[key]["B"] * 1e4, '.', color="C0", markersize=markersize)
+            plt.plot(ii, rates[key]["C"] * 1e4, '.', color="C1", markersize=markersize)
 
     for ii, key in enumerate(rates["rtt"].keys()):
-        plt.plot(2, rates["rtt"][key]*1e4, '.', color=colors[ii+2], markersize=markersize, label=key)
-    plt.plot(3, 19, '.', color="C0", markersize=markersize) # WH rate
-    plt.xticks(range(4), labels, fontsize=fontsize, rotation=8)
+        plt.plot(2, rates["rtt"][key] * 1e4, '.', color=cmap_colors[ii], markersize=markersize, label=key)
+
+    for ii, key in enumerate(["first", "second", "third"]):
+        plt.plot(3, rates["WH"]["any"]["global"]["all"][key] * 1e4, ".",
+                 color=colors[ii + 2], markersize=markersize)
+        plt.plot(4, rates["WH"]["subtypes"]["global"]["all"][key] * 1e4, ".",
+                 color=colors[ii + 2], markersize=markersize)
+        plt.plot(5, rates["WH"]["founder"]["global"]["all"][key] * 1e4, ".",
+                 color=colors[ii + 2], markersize=markersize, label=key)
+
+    plt.xticks(range(6), labels, fontsize=fontsize, rotation=8)
     plt.ylabel("Mutation rates (per year) * e-4", fontsize=fontsize)
     plt.legend(fontsize=fontsize)
     plt.grid()
-    plt.ylim([0, 20])
     plt.savefig("figures/mutation_rates.png", format="png")
     plt.show()
 
