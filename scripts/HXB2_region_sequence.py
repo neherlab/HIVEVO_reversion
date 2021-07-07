beec62b49118a4024b4a70466d1e32459913500c
Valentin <valentin.druelle@unibas.ch>
2021-07-07 11:45:43 +0200

    Script to create HXB2 region sequences for env pol and gag

---
+++ scripts/HXB2_region_sequence.py
@@ -0,0 +1,14 @@
+# Script to create the HXB2 sequences for each region (env pol and gag) from the HIVevo references
+import filenames
+from hivevo.patients import Patient
+from Bio import SeqIO
+import numpy as np
+
+if __name__ == '__main__':
+    ref_sequence = SeqIO.read("data/BH/reference/HXB2.fasta", format="fasta")
+    for region in ["env", "pol", "gag"]:
+        patient = Patient.load("p1")  # choice of patient is irrelevant for this
+        map = patient.map_to_external_reference(region)
+        region_seq = ref_sequence[map[0, 0]:map[-1, 0]]
+        with open(f"data/BH/reference/HXB2_{region}.fasta", "w") as f:
+            SeqIO.write(region_seq, f, "fasta")
