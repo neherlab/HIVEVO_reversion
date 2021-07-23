bf3c7a16dc83f249937aeeac2feb93d0f5b6e862
Valentin <valentin.druelle@unibas.ch>
2021-07-23 11:56:44 +0200

    3rd and 4th figure addition

--- scripts/Paper_figures.py
+++ scripts/Paper_figures.py
@@ -2,6 +2,8 @@ from distance_in_time import get_reference_sequence, get_mean_distance_in_time, 
 import divergence
 import numpy as np
 import matplotlib.pyplot as plt
+import trajectory
+import json
 plt.style.use("tex")
 
 
@@ -69,7 +71,7 @@ def make_figure_1(region, text_pos, ylim, sharey, savefig=False):
     axs[0].ticklabel_format(axis="x", style="plain")
 
     # WH plot references
-    idxs = time < 5.3
+    idxs = time < 5.3  # Time around which some patients stop being followed
     time = time[idxs]
     keys = ["root", "subtypes", "founder"]
     for key in keys:
@@ -92,12 +94,218 @@ def make_figure_1(region, text_pos, ylim, sharey, savefig=False):
         fig.savefig(f"figures/Distance_{region}.pdf")
 
 
-def make_figure_2():
-    return
+def make_figure_2(region, text, savefig=False):
+    fill_alpha = 0.15
+    colors = ["C3", "C0", "C1", "C2", "C4", "C5", "C6", "C7", "C8", "C9"]
+    lines = ["-", "-", "--"]
+    figsize = (6.7315, 3)
+
+    div_dict = divergence.load_div_dict("data/WH/bootstrap_div_dict.json")
+    time = div_dict["time"]
+    idxs = time < 5.3  # Time around which some patients stop being followed
+    time = time[idxs]
+
+    fig, axs = plt.subplots(ncols=2, nrows=1, figsize=figsize, sharey=True, sharex=True)
+    # Left plot
+    labels = ["all", "consensus", "non-consensus"]
+    for ii, key in enumerate(["all", "consensus", "non_consensus"]):
+        data = div_dict[region]["founder"]["global"][key]["all"]["mean"][idxs]
+        std = div_dict[region]["founder"]["global"][key]["all"]["std"][idxs]
+
+        axs[0].plot(time, data, lines[ii], color=colors[ii], label=labels[ii])
+        axs[0].fill_between(time, data + std, data - std, color=colors[ii], alpha=fill_alpha)
+
+    axs[0].text(text[0][1][0], text[0][1][1], text[0][0], color=colors[1])
+    axs[0].text(text[1][1][0], text[1][1][1], text[1][0], color=colors[2])
+
+    axs[0].set_xlabel("Time [years]")
+    axs[0].set_ylabel("Divergence from founder")
+    axs[0].legend()
+    axs[0].set_xlim([-0.3, 5.5])
+
+    # Right plot
+    for ii, key in enumerate(["consensus", "non_consensus"]):
+        for jj, key2 in enumerate(["first", "second", "third"]):
+            data = div_dict["pol"]["founder"]["global"][key][key2]["mean"][idxs]
+            std = div_dict[region]["founder"]["global"][key][key2]["std"][idxs]
+            axs[1].plot(time, data, lines[ii + 1], color=colors[jj + 3])
+            axs[1].fill_between(time, data + std, data - std, color=colors[jj + 3], alpha=fill_alpha)
+    axs[1].text(text[2][1][0], text[2][1][1], text[2][0], color=colors[3])
+    axs[1].text(text[3][1][0], text[3][1][1], text[3][0], color=colors[4])
+    axs[1].text(text[4][1][0], text[4][1][1], text[4][0], color=colors[5])
+
+    axs[1].set_xlabel("Time [years]")
+
+    # Legend
+    for ii, label in enumerate(["consensus", "non-consensus"]):
+        axs[1].plot([0], [0], lines[ii + 1], color="k", label=label)
+    for jj, label in enumerate(["1st", "2nd", "3rd"]):
+        axs[1].plot([0], [0], lines[0], color=colors[jj + 3], label=label)
+
+    axs[1].legend()
+
+    if savefig:
+        plt.savefig(f"figures/Divergence_details_{region}.pdf")
+
+
+def make_figure_3(savegif=False):
+    reference = "any"  # "any" or "subtypes"
+    fill_alpha = 0.15
+    figsize = (6.7315, 3)
+    colors = ["C0", "C1", "C2", "C4"]
+
+    trajectory_file = f"data/WH/Trajectory_list_{reference}.json"
+    mean_in_time_file = f"data/WH/bootstrap_mean_dict_{reference}.json"
+
+    # Data loading
+    trajectories = trajectory.load_trajectory_list(trajectory_file)
+    times = trajectory.create_time_bins(400)
+    times = 0.5 * (times[:-1] + times[1:]) / 365  # In years
+    bootstrap_dict = trajectory.load_mean_in_time_dict(mean_in_time_file)
+
+    # Selecting reversion trajectories in [0.4, 0.6] for left pannel
+    freq_ranges = [[0.2, 0.4], [0.4, 0.6], [0.6, 0.8]]
+    trajectories_scheme = [traj for traj in trajectories if traj.reversion]
+    trajectories_scheme = trajectory.offset_trajectories(trajectories_scheme, [0.4, 0.6])
+
+    fig, axs = plt.subplots(ncols=2, nrows=1, figsize=figsize, sharey=True, sharex=True)
+
+    # Plot left
+    for traj in trajectories_scheme:
+        axs[0].plot(traj.t / 365, traj.frequencies, "k-", alpha=0.2, linewidth=0.5)
+
+    mean = bootstrap_dict["rev"]["[0.4, 0.6]"]["mean"]
+    std = bootstrap_dict["rev"]["[0.4, 0.6]"]["std"]
+    axs[0].plot(times, mean, '-', color=colors[1])
+    axs[0].fill_between(times, mean - std, mean + std, color=colors[1], alpha=fill_alpha)
+
+    axs[0].set_xlabel("Time [years]")
+    axs[0].set_ylabel("Frequency")
+    axs[0].set_ylim([-0.03, 1.03])
+    axs[0].set_xlim([-2, 8.5])
+
+    line1, = axs[0].plot([0], [0], "k-")
+    line2, = axs[0].plot([0], [0], "-", color=colors[1])
+    axs[0].legend([line1, line2], ["Individual trajectories", "Average"], loc="lower right")
+
+    # Plot right
+    for ii, freq_range in enumerate(freq_ranges):
+        for key, line in zip(["rev", "non_rev"], ["-", "--"]):
+            mean = bootstrap_dict[key][str(freq_range)]["mean"]
+            std = bootstrap_dict[key][str(freq_range)]["std"]
+            axs[1].plot(times, mean, line, color=colors[ii])
+            axs[1].fill_between(times, mean - std, mean + std, color=colors[ii], alpha=fill_alpha)
+
+    line1, = axs[1].plot([0], [0], "k-")
+    line2, = axs[1].plot([0], [0], "k--")
+    line3, = axs[1].plot([0], [0], "-", color=colors[0])
+    line4, = axs[1].plot([0], [0], "-", color=colors[1])
+    line5, = axs[1].plot([0], [0], "-", color=colors[2])
+
+    axs[1].set_xlabel("Time [years]")
+    axs[1].set_ylim([-0.03, 1.03])
+    axs[1].legend([line3, line4, line5, line1, line2],
+                  ["[0.2, 0.4]", "[0.4, 0.6]", "[0.6, 0.8]", "reversion", "non-reversion"],
+                  ncol=2, loc="lower right")
+
+    if savefig:
+        plt.savefig(f"figures/mean_in_time_{reference}.pdf")
+
+
+def compute_rates(region):
+    """
+    Returns the mutation rates from the distance to root, distance to subtype consensus and root to tip
+    distance.
+    """
+    # BH rate files
+    reference_file = {}
+    reference_file["root"] = f"data/BH/intermediate_files/{region}_1000_nt_muts.json"
+    reference_file["B"] = f"data/BH/alignments/to_HXB2/{region}_1000_B_consensus.fasta"
+    reference_file["C"] = f"data/BH/alignments/to_HXB2/{region}_1000_C_consensus.fasta"
+    alignment_file = f"data/BH/alignments/to_HXB2/{region}_1000.fasta"
+    tree_file = f"data/BH/intermediate_files/timetree_{region}_1000.nwk"
+    branch_length_file = f"data/BH/intermediate_files/branch_lengths_{region}_1000.json"
+
+    # BH GTR files
+    gtr_file = f"data/BH/mutation_rates/{region}_1000.json"
+
+    # Rates from hamming distance
+    rates = {"root": {}, "subtypes": {}}
+
+    reference_sequence = get_reference_sequence(reference_file["root"])
+    for subtype in ["B", "C"]:
+        rates["root"][subtype] = {}
+        years, dist, std, _ = get_mean_distance_in_time(alignment_file, reference_sequence, subtype)
+        for key in ["first", "second", "third", "all"]:
+            fit = np.polyfit(years, dist[key], deg=1)
+            rates["root"][subtype][key] = fit[0]
+
+    for subtype in ["B", "C"]:
+        reference_sequence = get_reference_sequence(reference_file[subtype])
+        rates["subtypes"][subtype] = {}
+        years, dist, std, _ = get_mean_distance_in_time(alignment_file, reference_sequence, subtype)
+        for key in ["first", "second", "third", "all"]:
+            fit = np.polyfit(years, dist[key], deg=1)
+            rates["subtypes"][subtype][key] = fit[0]
+
+    # Rates from root to tip distance
+    dates, lengths = get_root_to_tip_distance(tree_file, branch_length_file)
+    fit = np.polyfit(dates, lengths, deg=1)
+    rates["rtt"] = fit[0]
+
+    # BH rates from GTR estimates
+    with open(gtr_file) as f:
+        rates["GTR"] = json.load(f)
+
+    # WH rates
+    WH_file = "data/WH/avg_rate_dict.json"
+    WH_rate_dict = divergence.load_avg_rate_dict(WH_file)
+    rates["WH"] = WH_rate_dict["pol"]
+
+    return rates
+
+
+def make_figure_4(region, savefig=False):
+    """
+    Plot for the mutation rates.
+    """
+    markersize = 5
+    colors = ["k", "C0", "C1", "C2", "C3", "C4", "C5", "C6"]
+    labels = ["H-root", "H-subtype", "RTT", "GTR", "WH_root", "WH_subtypes", "WH_founder"]
+
+    rates = compute_rates(region)
+
+    plt.figure()
+    # BH stuff
+    for ii, key in enumerate(["root", "subtypes"]):
+        # for jj, key2 in enumerate(["all", "first", "second", "third"]):
+        for jj, key2 in enumerate(["first", "second", "third", "all"]):
+            plt.plot(ii, rates[key]["B"][key2], 'o', color=colors[jj])
+
+    plt.plot(2, rates["rtt"], 'o', color=colors[0], markersize=markersize)
+
+    for ii, key in enumerate(rates["GTR"].keys()):
+        plt.plot(3, rates["GTR"][key], "o", color=colors[ii], markersize=markersize, label=key)
+
+    # WH stuff
+    for ii, key in enumerate(["all", "first", "second", "third"]):
+        plt.plot(4, rates["WH"]["root"]["global"]["all"][key], 'o',
+                 color=colors[ii], markersize=markersize)
+        plt.plot(5, rates["WH"]["subtypes"]["global"]["all"][key], 'o',
+                 color=colors[ii], markersize=markersize)
+        plt.plot(6, rates["WH"]["founder"]["global"]["all"][key], 'o',
+                 color=colors[ii], markersize=markersize)
+
+    plt.xticks(range(len(labels)), labels, rotation=14)
+    plt.ylabel("Mutation rates")
+    plt.legend()
+    if savefig:
+        plt.savefig(f"figures/Rates_{region}.pdf")
+    plt.show()
 
 
 if __name__ == '__main__':
-    fig1, fig2, fig3, fig4 = False, True, False, False
+    fig1, fig2, fig3, fig4 = False, False, False, True
     savefig = False
 
     if fig1:
@@ -112,62 +320,18 @@ if __name__ == '__main__':
             make_figure_1(region, text[region], ylim[region], sharey[region], savefig=savefig)
 
     if fig2:
-        region = "pol"
-        fill_alpha = 0.15
-        colors = ["C3", "C0", "C1", "C2", "C4", "C5", "C6", "C7", "C8", "C9"]
-        lines = ["-", "-", "--"]
-        figsize = (6.7315, 3)
+        # from the fraction_consensus.py file
+        text = {"env": [("92%", [4.1, 0.003]), ("8%", [4.1, 0.062]), ("7%", [4.1, 0.018]),
+                        ("6%", [4.1, 0.09]), ("12%", [4.1, 0.058])],
+                "pol": [("94%", [4.1, -0.002]), ("6%", [4.1, 0.054]), ("4%", [4.1, 0.048]),
+                        ("2%", [4.1, 0.111]), ("11%", [4.1, 0.023])],
+                "gag": [("93%", [4.1, 0.001]), ("7%", [4.1, 0.078]), ("5%", [4.1, 0.051]),
+                        ("4%", [4.1, 0.09]), ("11%", [4.1, 0.029])]}
+        for region in ["env", "pol", "gag"]:
+            make_figure_2(region, text[region], savefig)
 
-        text = [("8%", [4.1, -0.002]), ("92%", [4.1, 0.054]),
-                ("4%", [4.1, 0.048]), ("2%", [4.1, 0.111]), ("11%", [4.1, 0.023])]
-        # text = {"env": [],
-        #         "pol": [("test", [-0.002, 4.5]), ("test2", [0.061, 0.041])],
-        #         "gag": []}
-
-        div_dict = divergence.load_div_dict("data/WH/bootstrap_div_dict.json")
-        time = div_dict["time"]
-        idxs = time < 5.3
-        time = time[idxs]
-
-        fig, axs = plt.subplots(ncols=2, nrows=1, figsize=figsize, sharey=True, sharex=True)
-        # Left plot
-        for ii, key in enumerate(["all", "consensus", "non_consensus"]):
-            data = div_dict[region]["founder"]["global"][key]["all"]["mean"][idxs]
-            std = div_dict[region]["founder"]["global"][key]["all"]["std"][idxs]
-
-            axs[0].plot(time, data, lines[ii], color=colors[ii], label=f"{key}")
-            axs[0].fill_between(time, data + std, data - std, color=colors[ii], alpha=fill_alpha)
-
-        axs[0].text(text[0][1][0], text[0][1][1], text[0][0], color=colors[1])
-        axs[0].text(text[1][1][0], text[1][1][1], text[1][0], color=colors[2])
-
-        axs[0].set_xlabel("Time [years]")
-        axs[0].set_ylabel("Divergence from founder")
-        axs[0].legend()
-        axs[0].set_xlim([-0.3, 5.5])
-
-        # Right plot
-        for ii, key in enumerate(["consensus", "non_consensus"]):
-            for jj, key2 in enumerate(["first", "second", "third"]):
-                data = div_dict["pol"]["founder"]["global"][key][key2]["mean"][idxs]
-                std = div_dict[region]["founder"]["global"][key][key2]["std"][idxs]
-                axs[1].plot(time, data, lines[ii + 1], color=colors[jj + 3])
-                axs[1].fill_between(time, data + std, data - std, color=colors[jj + 3], alpha=fill_alpha)
-        axs[1].text(text[2][1][0], text[2][1][1], text[2][0], color=colors[3])
-        axs[1].text(text[3][1][0], text[3][1][1], text[3][0], color=colors[4])
-        axs[1].text(text[4][1][0], text[4][1][1], text[4][0], color=colors[5])
-
-        axs[1].set_xlabel("Time [years]")
-
-        # Legend
-        for ii, label in enumerate(["consensus", "non-consensus"]):
-            axs[1].plot([0], [0], lines[ii + 1], color="k", label=label)
-        for jj, label in enumerate(["1st", "2nd", "3rd"]):
-            axs[1].plot([0], [0], lines[0], color=colors[jj + 3], label=label)
-
-        axs[1].legend()
-
-        if savefig:
-            plt.savefig(f"figures/Divergence_details_{region}.pdf")
-
+    if fig3:
+        make_figure_3(savefig)
+    if fig4:
+        make_figure_4("pol", savefig)
     plt.show()
