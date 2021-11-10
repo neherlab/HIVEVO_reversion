82bab565ef79f9ff4741f8b256fb756ff1edf55d
Valentin <valentin.druelle@unibas.ch>
2021-11-10 15:50:09 +0100

    RTT distance analysis

--- scripts/gtr_modeling.py
+++ scripts/gtr_modeling.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from treetime import TreeTime
 from treetime.gtr_site_specific import GTR_site_specific
 from treetime.seqgen import SeqGen
-from treetime.treeanc import TreeAnc
+from treetime.utils import parse_dates
 from Bio import AlignIO
 from Bio.Seq import Seq
 from Bio import Phylo
@@ -71,38 +72,117 @@ def get_ATGC_content(alignment):
     return prop
 
 
-def compare_ATGC_distributions(original_MSA, generated_MSA):
+def compare_ATGC_distributions(original_MSA, generated_MSA, nucleotides=["A", "T", "G", "C"],
+                               colors=["C0", "C1", "C2", "C3"]):
     """
     Plots the distribution for the original and generated proportion of ATGC from the MSA.
     """
     ATGC_original = get_ATGC_content(original_MSA)
     ATGC_generated = get_ATGC_content(generated_MSA)
 
+    plt.figure()
     for ii in range(4):
-        hist_or, bins = np.histogram(ATGC_original[:, ii], bins=200, range=[0, 0.5])
-        hist_gen, _ = np.histogram(ATGC_generated[:, ii], bins=200, range=[0, 0.5])
+        hist_or, bins = np.histogram(ATGC_original[:, ii], bins=500, range=[0, 0.5])
+        hist_gen, _ = np.histogram(ATGC_generated[:, ii], bins=500, range=[0, 0.5])
         bins = 0.5 * (bins[:-1] + bins[1:])
 
-        plt.figure()
-        plt.title(["A", "T", "G", "C"][ii])
-        plt.plot(bins, hist_or, '-', label="orginal")
-        plt.plot(bins, hist_gen, '--', label="generated")
-        plt.legend()
-        plt.grid()
-        plt.xlabel("Frequency")
-        plt.ylabel("Counts")
+        plt.plot(bins, hist_or, '-', color=colors[ii], label=f"orginal {nucleotides[ii]}")
+        plt.plot(bins, hist_gen, '--', color=colors[ii], label=f"generated {nucleotides[ii]}")
+    plt.legend()
+    plt.grid()
+    plt.xlabel("Frequency")
+    plt.ylabel("Counts")
     plt.show()
 
 
+def get_hamming_distance(MSA, reference_sequence):
+    """
+    Computes the hamming distance of each sequence in the MSA to the reference sequence provided.
+    """
+    distance = np.zeros(MSA.shape[0])
+    for ii in range(MSA.shape[0]):
+        distance[ii] = np.sum(MSA[ii] != reference_sequence, dtype=np.float32) / MSA.shape[1]
+    return distance
+
+
+def compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, title=""):
+    """
+    Computes and plots the distribution of hamming distances to the reference sequence.
+    """
+    distance_original = get_hamming_distance(original_MSA, ref_seq)
+    distance_generated = get_hamming_distance(generated_MSA, ref_seq)
+
+    plt.figure()
+    plt.title(title)
+    hist_or, bins = np.histogram(distance_original, bins=500, range=[0, 0.5])
+    hist_gen, _ = np.histogram(distance_generated, bins=500, range=[0, 0.5])
+    bins = 0.5 * (bins[:-1] + bins[1:])
+
+    plt.plot(bins, hist_or, '-', label=f"orginal")
+    plt.plot(bins, hist_gen, '-', label=f"generated")
+    plt.legend()
+    plt.grid()
+    plt.xlabel("Percentage difference")
+    plt.ylabel("Counts")
+    plt.show()
+
+
+def get_RTT(tree):
+    """
+    Return the RTT distance and dates for each tip in the tree.
+    """
+    tips = tree.get_terminals()
+    dates = []
+    for tip in tips:
+        date = tip.name.split(".")[2]
+        dates += [int(date)]
+
+    rtt = [tree.distance(tip) for tip in tips]
+
+    return np.array(rtt), np.array(dates)
+
+
 if __name__ == "__main__":
-    original_MSA = "data/BH/alignments/to_HXB2/pol_1000.fasta"
-    generated_MSA = "data/modeling/generated_MSA/homogeneous.fasta"
-    original_tree = "data/BH/intermediate_files/tree_pol_1000.nwk"
-    generated_tree = "data/modeling/generated_trees/homogeneous.nwk"
+    original_MSA_path = "data/BH/alignments/to_HXB2/pol_1000.fasta"
+    generated_MSA_path = "data/modeling/generated_MSA/homogeneous.fasta"
+    original_tree_path = "data/BH/intermediate_files/tree_pol_1000.nwk"
+    generated_tree_path = "data/modeling/generated_trees/homogeneous.nwk"
+    root_path = "data/BH/intermediate_files/pol_1000_nt_muts.json"
+    consensus_path = "data/BH/alignments/to_HXB2/pol_1000_consensus.fasta"
+    original_metadata_path = "data/BH/raw/pol_1000_subsampled_metadata.tsv"
 
-    original_MSA = AlignIO.read(original_MSA, "fasta")
-    original_MSA = np.array(original_MSA)
-    generated_MSA = AlignIO.read(generated_MSA, "fasta")
-    generated_MSA = np.array(generated_MSA)
+    original_MSA = AlignIO.read(original_MSA_path, "fasta")
+    original_MSA = np.array(original_MSA_path)
+    generated_MSA = AlignIO.read(generated_MSA_path, "fasta")
+    generated_MSA = np.array(generated_MSA_path)
 
-    compare_ATGC_distributions(original_MSA, generated_MSA)
+    # compare_ATGC_distributions(original_MSA, generated_MSA)
+    #
+    # ref_seq = get_reference_sequence(root_path)
+    # compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to root")
+    # ref_seq = get_reference_sequence(consensus_path)
+    # compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to consensus")
+
+    # tree_or = Phylo.read(original_tree_path, "newick")
+    # tree_gen = Phylo.read(generated_tree_path, "newick")
+
+    dates = parse_dates(original_metadata_path)
+    ttree = TreeTime(gtr='Jukes-Cantor', tree=original_tree_path,
+                     precision=1, aln=original_MSA_path, verbose=2, dates=dates)
+    ttree.reroot()
+    tree_or = ttree._tree
+    ttree = TreeTime(gtr='Jukes-Cantor', tree=generated_tree_path,
+                     precision=1, aln=generated_MSA_path, verbose=2, dates=dates)
+    ttree.reroot()
+    tree_gen = ttree._tree
+
+    rtt_or, dates_or = get_RTT(tree_or)
+    rtt_gen, dates_gen = get_RTT(tree_gen)
+
+    plt.figure()
+    plt.plot(dates_or, rtt_or, '.', label="original")
+    # plt.plot(dates_gen, rtt_gen, '.', label="generated")
+    plt.legend()
+    plt.grid()
+    plt.xlabel("Years")
+    plt.ylabel("RTT")
