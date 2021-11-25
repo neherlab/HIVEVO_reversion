9902b8f1e134abcdcf0e4db6714dda8a42d79db2
Valentin <valentin.druelle@unibas.ch>
2021-11-25 13:28:47 +0100

    Added binary p

--- scripts/gtr_modeling.py
+++ scripts/gtr_modeling.py
@@ -26,17 +26,42 @@ def homogeneous_p(consensus_seq, r_minus, r_plus):
     return p
 
 
-def SeqGen_homogeneous():
+def transition_idx(idx):
+    "Returns the indice corresponding to the transition mutation from the given idx."
+    transi_list = [2, 3, 0, 1, 5, 6]
+    return transi_list[idx]
+
+
+def binary_p(consensus_seq, r_minus, r_plus):
     """
-    Generates an MSA based on a homogeneous (same for all site) model for the reversion and non-reversion rates.
+    Returns a p matrix of shape (4*len(consensus_seq)) where the equilibrium frequencies are set
+    according to the global reversion and non-reversion mutation rate.
     """
-    tree_path = "data/BH/intermediate_files/tree_pol_1000.nwk"
-    root_path = "data/BH/intermediate_files/pol_1000_nt_muts.json"
-    consensus_path = "data/BH/alignments/to_HXB2/pol_1000_consensus.fasta"
+    p = np.zeros((4, len(consensus_seq)))
+    consensus_idxs = tools.sequence_to_indices(consensus_seq)
+    for ii in range(len(consensus_seq)):
+        cons_fraction = r_minus / (r_plus + r_minus)
+        p[consensus_idxs[ii], ii] = cons_fraction
+        p[transition_idx(consensus_idxs[ii]), ii] = 1 - cons_fraction
+    return p
+
+
+def Generate_data(tree_path, root_path, consensus_path, MSA, metadata, p_type="homogeneous"):
+    """
+    Generates an MSA based on a homogeneous (same for all site) model for the reversion and non-reversion
+    rates.
+    """
+    assert p_type in ["homogeneous", "binary"], f"p_type must be 'homogeneous' or 'binary', got {p_type}"
 
     root_seq = get_reference_sequence(root_path)
     root_seq = Seq("".join(root_seq))
+
     tree = Phylo.read(tree_path, "newick")
+    dates = parse_dates(metadata)
+    ttree = TreeTime(gtr='Jukes-Cantor', tree=tree, precision=1, aln=MSA, verbose=2, dates=dates)
+    ttree.reroot()
+    tree = ttree._tree
+
     consensus_seq = get_reference_sequence(consensus_path)
     # Replaces N by A, it's not correct but I have a 4 letter aphabet for now
     consensus_seq[consensus_seq == "N"] = "A"
@@ -45,18 +70,23 @@ def SeqGen_homogeneous():
     reversion_rate = 0.009270115831081912
 
     L = len(root_seq)  # sequence length
-    N = 4  # alphabet size
     mu = np.ones(L)
     W = np.array([[0., 0.763, 2.902, 0.391],
                   [0.763, 0., 0.294, 3.551],
                   [2.902, 0.294, 0., 0.317],
                   [0.391, 3.551, 0.317, 0.]])
-    p = homogeneous_p(consensus_seq, reversion_rate, non_reversion_rate)
+
+    if p_type == "homogeneous":
+        p = homogeneous_p(consensus_seq, reversion_rate, non_reversion_rate)
+    elif p_type == "binary":
+        p = binary_p(consensus_seq, reversion_rate, non_reversion_rate)
 
     myGTR = GTR_site_specific.custom(mu, p, W, alphabet="nuc_nogap")
+    myGTR.mu /= myGTR.average_rate().mean()
     MySeq = SeqGen(3012, gtr=myGTR, tree=tree)
     MySeq.evolve(root_seq=root_seq)
-    with open("data/modeling/homogeneous/generated_MSA/homogeneous.fasta", "wt") as f:
+    # MySeq.evolve()
+    with open("data/modeling/generated_MSA/homogeneous.fasta", "wt") as f:
         AlignIO.write(MySeq.get_aln(), f, "fasta")
 
 
@@ -92,7 +122,6 @@ def compare_ATGC_distributions(original_MSA, generated_MSA, nucleotides=["A", "T
     plt.grid()
     plt.xlabel("Frequency")
     plt.ylabel("Counts")
-    plt.show()
 
 
 def get_hamming_distance(MSA, reference_sequence):
@@ -124,7 +153,6 @@ def compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, title=""
     plt.grid()
     plt.xlabel("Percentage difference")
     plt.ylabel("Counts")
-    plt.show()
 
 
 def get_RTT(tree):
@@ -142,37 +170,18 @@ def get_RTT(tree):
     return np.array(rtt), np.array(dates)
 
 
-if __name__ == "__main__":
-    original_MSA_path = "data/BH/alignments/to_HXB2/pol_1000.fasta"
-    generated_MSA_path = "data/modeling/generated_MSA/homogeneous.fasta"
-    original_tree_path = "data/BH/intermediate_files/tree_pol_1000.nwk"
-    generated_tree_path = "data/modeling/generated_trees/homogeneous.nwk"
-    root_path = "data/BH/intermediate_files/pol_1000_nt_muts.json"
-    consensus_path = "data/BH/alignments/to_HXB2/pol_1000_consensus.fasta"
-    original_metadata_path = "data/BH/raw/pol_1000_subsampled_metadata.tsv"
-
-    original_MSA = AlignIO.read(original_MSA_path, "fasta")
-    original_MSA = np.array(original_MSA_path)
-    generated_MSA = AlignIO.read(generated_MSA_path, "fasta")
-    generated_MSA = np.array(generated_MSA_path)
-
-    # compare_ATGC_distributions(original_MSA, generated_MSA)
-    #
-    # ref_seq = get_reference_sequence(root_path)
-    # compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to root")
-    # ref_seq = get_reference_sequence(consensus_path)
-    # compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to consensus")
-
-    # tree_or = Phylo.read(original_tree_path, "newick")
-    # tree_gen = Phylo.read(generated_tree_path, "newick")
-
-    dates = parse_dates(original_metadata_path)
-    ttree = TreeTime(gtr='Jukes-Cantor', tree=original_tree_path,
-                     precision=1, aln=original_MSA_path, verbose=2, dates=dates)
+def compare_RTT(tree_or, MSA_or, tree_gen, MSA_gen, metadata):
+    """
+    Returns a p matrix of shape (4*len(consensus_seq)) where the equilibrium frequencies are set
+    according to the global reversion and non-reversion mutation rate.
+    """
+    dates = parse_dates(metadata)
+    ttree = TreeTime(gtr='Jukes-Cantor', tree=tree_or,
+                     precision=1, aln=MSA_or, verbose=2, dates=dates)
     ttree.reroot()
     tree_or = ttree._tree
-    ttree = TreeTime(gtr='Jukes-Cantor', tree=generated_tree_path,
-                     precision=1, aln=generated_MSA_path, verbose=2, dates=dates)
+    ttree = TreeTime(gtr='Jukes-Cantor', tree=tree_gen,
+                     precision=1, aln=MSA_gen, verbose=2, dates=dates)
     ttree.reroot()
     tree_gen = ttree._tree
 
@@ -181,8 +190,95 @@ if __name__ == "__main__":
 
     plt.figure()
     plt.plot(dates_or, rtt_or, '.', label="original")
-    # plt.plot(dates_gen, rtt_gen, '.', label="generated")
+    plt.plot(dates_gen, rtt_gen, '.', label="generated")
     plt.legend()
     plt.grid()
     plt.xlabel("Years")
     plt.ylabel("RTT")
+
+    # def site_mask(consensus_seq, position):
+    #     """
+    #     Returns a bolean of shape len(consensus_seq) where True are the sites corresponding to the given
+    #     position (first, second or third)
+    #     """
+    #     assert position in [1, 2, 3], "Position must be 1 2 or 3."
+    #     position_mask = np.zeros(consensus_seq.shape[-1], dtype=bool)
+    #     position_mask[position - 1::3] = True
+    #     return position_mask
+    #
+    # def consensus_mask(consensus_seq):
+    #     """
+    #     Returns a bolean of shape 4*len(consensus_seq) where True are the position that correspond to the
+    #     consensus sequence.
+    #     """
+    #     mask = np.zeros((4, len(consensus_seq)), dtype=bool)
+    #     consensus_idxs = tools.sequence_to_indices(consensus_seq)
+    #
+    #     for ii in range(len(consensus_seq)):
+    #         mask[consensus_idxs[ii], ii] = True
+    #     return mask
+    #
+    # def p_consensus(rate_consensus, rate_non_consensus):
+    #     return rate_non_consensus / (3 * rate_consensus + rate_non_consensus)
+    #
+    # def p_non_consensus(rate_consensus, rate_non_consensus):
+    #     return (1 - (rate_non_consensus / (3 * rate_consensus + rate_non_consensus))) / 3
+    #
+    # import divergence
+    # rates = divergence.load_avg_rate_dict("data/WH/avg_rate_dict.json")
+    # rates = rates["pol"]["founder"]["global"]
+    # consensus_seq = get_reference_sequence(consensus_path)
+    # consensus_seq[consensus_seq == "N"] = "A"
+    #
+    # mask_first = site_mask(consensus_seq, 1)
+    # mask_second = site_mask(consensus_seq, 2)
+    # mask_third = site_mask(consensus_seq, 3)
+    # mask_consensus = consensus_mask(consensus_seq)
+    #
+    # p = np.zeros((4, len(consensus_seq)))
+    # p[:, mask_first] = p_non_consensus(rates["consensus"]["first"]["rate"],
+    #                                    rates["non_consensus"]["first"]["rate"])
+    # p[:, mask_second] = p_non_consensus(rates["consensus"]["second"]["rate"],
+    #                                     rates["non_consensus"]["second"]["rate"])
+    # p[:, mask_third] = p_non_consensus(rates["consensus"]["third"]["rate"],
+    #                                    rates["non_consensus"]["third"]["rate"])
+    # p[np.logical_and(mask_first, mask_consensus)] = p_consensus(rates["consensus"]["first"]["rate"],
+    #                                                             rates["non_consensus"]["first"]["rate"])
+    # p[np.logical_and(mask_second, mask_consensus)] = p_consensus(rates["consensus"]["second"]["rate"],
+    #                                                              rates["non_consensus"]["second"]["rate"])
+    # p[np.logical_and(mask_third, mask_consensus)] = p_consensus(rates["consensus"]["third"]["rate"],
+    #                                                             rates["non_consensus"]["third"]["rate"])
+
+
+if __name__ == "__main__":
+    original_MSA_path = "data/BH/alignments/to_HXB2/pol_1000.fasta"
+    generated_MSA_path = "data/modeling/generated_MSA/homogeneous.fasta"
+    # generated_MSA_path = "data/modeling/generated_MSA/homogeneous_consensus.fasta"
+    original_tree_path = "data/BH/intermediate_files/tree_pol_1000.nwk"
+    generated_tree_path = "data/modeling/generated_trees/homogeneous.nwk"
+    # generated_tree_path = "data/modeling/generated_trees/homogeneous_consensus.nwk"
+    root_path = "data/BH/intermediate_files/pol_1000_nt_muts.json"
+    consensus_path = "data/BH/alignments/to_HXB2/pol_1000_consensus.fasta"
+    original_metadata_path = "data/BH/raw/pol_1000_subsampled_metadata.tsv"
+    regenerate = False
+
+    if regenerate:
+        Generate_data(original_tree_path, root_path, consensus_path,
+                      original_MSA_path, original_metadata_path)
+
+    original_MSA = AlignIO.read(original_MSA_path, "fasta")
+    original_MSA = np.array(original_MSA)
+    generated_MSA = AlignIO.read(generated_MSA_path, "fasta")
+    generated_MSA = np.array(generated_MSA)
+
+    compare_ATGC_distributions(original_MSA, generated_MSA)
+
+    ref_seq = get_reference_sequence(root_path)
+    compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to root")
+    ref_seq = get_reference_sequence(consensus_path)
+    compare_hamming_distributions(original_MSA, generated_MSA, ref_seq, "to consensus")
+
+    compare_RTT(original_tree_path, original_MSA_path,
+                generated_tree_path, generated_MSA_path, original_metadata_path)
+
+    plt.show()
--- scripts/test.py
+++ scripts/test.py
@@ -16,156 +16,59 @@ from Bio import AlignIO
 from Bio.Seq import Seq
 
 
-import numpy as np
-import random as rand
-import scipy.special as sf
-from Bio import Phylo
-from distance_in_time import get_reference_sequence
+def non_consensus_percentages():
+    # Loading data
+    MSA_path = "data/BH/alignments/to_HXB2/pol_1000.fasta"
+    consensus_path = "data/BH/alignments/to_HXB2/pol_1000_consensus.fasta"
+    msa = AlignIO.read(MSA_path, "fasta")
+    msa = np.array(msa)
+    consensus = AlignIO.read(consensus_path, "fasta")
+    consensus = np.array(consensus).squeeze()
+
+    # Creating frequency array
+    af = np.zeros((5, consensus.shape[0]), dtype=int)
+    for ii, nuc in enumerate(["A", "T", "G", "C", "N"]):
+        af[ii, :] = np.sum(msa == nuc, axis=0)
+    af = af / msa.shape[0]
+
+    means = {}
+    for ii, nuc in enumerate(["A", "T", "G", "C", "N"]):
+        mask = consensus == nuc
+        tmp = af[:, mask]
+        means[nuc] = np.mean(tmp, axis=1)
+    print(means)
 
 
-class betatree(object):
-    '''
-    class that simulates a beta coalescent tree
-    parameters:
-    sample_size -- number of leaves
-    alpha       -- parameter of the merger distribution 2 for Kingman, 1 for BSC
-    '''
+def non_consensus_to_non_consensus():
+    "Quick and dirty analysis to get an idea of how much reversion VS non-consensus to non-consensus happen"
+    region = "pol"
+    ref = HIVreference(subtype="any")
 
-    def __init__(self, sample_size, alpha=2):
-        self.alpha = alpha
-        self.n = sample_size
-        # auxillary arrays
-        self.k = np.arange(self.n + 1)
-        if alpha == 1:
-            self.inv_k = 1.0 / np.arange(1, self.n + 2)
-            self.inv_kkp1 = self.inv_k / np.arange(2, self.n + 3)
-            self.cum_sum_inv_kkp1 = np.array([0] + np.cumsum(self.inv_kkp1).tolist())
-        elif alpha > 1 and alpha < 2:
-            self.normalizer = 1.0 / sf.gamma(alpha) / sf.gamma(2 - alpha)
-            self.gamma_ratiom = np.exp(sf.gammaln(self.k - self.alpha) - sf.gammaln(self.k + 1))
-            self.gamma_ratiop = np.exp(sf.gammaln(self.k + self.alpha) - sf.gammaln(self.k + 1))
+    patient_names = ["p1", "p2", "p3", "p4", "p5", "p6", "p8", "p9", "p11"]
+    full_change = []
+    non_reversion_change = []
+    for patient_name in patient_names:
+        patient = Patient.load(patient_name)
+        aft = patient.get_allele_frequency_trajectories(region)
+        non_consensus_mask = tools.non_reference_mask(patient, region, aft, ref)
+        reversion_map = tools.reversion_map(patient, region, aft, ref)
+        initial_idx = patient.get_initial_indices(region)
 
-    def init_tree(self):
-        '''
-        constructs the blocks that are to be merged, each leave corresponds
-        to a BioPython clade object
-        '''
-        self.blocks = [Phylo.BaseTree.Clade(name=str(i), branch_length=0)
-                       for i in range(self.n)]
+        # Set initial nucleotide to 0 frequency
+        aft[np.arange(aft.shape[0])[:, np.newaxis, np.newaxis], initial_idx, np.arange(aft.shape[-1])] = 0
+        tmp = aft[:, :, non_consensus_mask]
+        tmp = np.sum(tmp, axis=1)
+        full_change += [np.sum(tmp[-1, :])]
 
-    def coalescence_event(self):
-        '''
-        choose the time of the next merger,the number of blocks to merge,
-        and perform the merger
-        '''
-        merger_size = self.whichp(len(self.blocks))
-        waiting_time = self.waiting_time()
-        # branch length is first accumulated. once the tree is done, take
-        # differentials of parent and child branchlength to get the actual
-        # branchlength
-        for clade in self.blocks:
-            clade.branch_length += waiting_time
+        aft[:, reversion_map] = 0
+        tmp = aft[:, :, non_consensus_mask]
+        tmp = np.sum(tmp, axis=1)
+        non_reversion_change += [np.sum(tmp[-1, :])]
 
-        # randomly pick some blocks (there are (p choose k) possibilities)
-        merging_blocks = rand.sample(self.k[:len(self.blocks)].tolist(), merger_size)
-        self.merge_clades(merging_blocks)
-
-    def merge_clades(self, merging_blocks):
-        '''
-        creates a new clade whose children are the merging blocks
-        '''
-        # instantiate at Clade object with children given by the merging blocks
-        new_clade = Phylo.BaseTree.Clade(clades=[self.blocks[i]
-                                                 for i in merging_blocks])
-        # set the branch length to that of the children
-        new_clade.branch_length = self.blocks[merging_blocks[0]].branch_length
-        # remove the merging blocks from the active blocks
-        for i in sorted(merging_blocks, reverse=True):
-            self.blocks.pop(i)
-        self.blocks.append(new_clade)
-
-    def clean_up_subtree(self, clade):
-        '''
-        calculate the branch length and number of children for each node
-        '''
-        if clade.is_terminal():
-            clade.weight = 1
-            return
-        else:
-            clade.weight = 0
-            clade.branch_length -= clade.clades[0].branch_length
-            for child in clade.clades:
-                self.clean_up_subtree(child)
-                clade.weight += child.weight
-            return
-
-    def waiting_time(self):
-        '''
-        returns the waiting time to the next merger.
-        '''
-        b = len(self.blocks)
-        if self.alpha == 1:  # the BSC merger rate
-            # the BSC merger rate is simply b-1 = 1 / (sum_k 1/k(k-1))
-            dt = np.random.exponential(1.0 / (b - 1))
-        elif self.alpha == 2:  # the Kingman merger rate
-            dt = np.random.exponential(2.0 / b / (b - 1))
-        else:  # the general beta coalescent case
-            rate = b * (self.gamma_ratiom[2:b + 1] * self.gamma_ratiop[b - 2::-1]).sum() * self.normalizer
-            dt = np.random.exponential(1.0 / rate)  # this product gives the Beta coalescent merger rate
-        return dt
-
-    def whichp(self, b):
-        '''
-        generates the merger size distribution, then samples from it.
-        parameters:
-            b: the number of extant lineages.
-        '''
-        if self.alpha == 1:  # BSC case
-            # merger degree is distributed as 1/(k-1)/k, pull a random number from
-            # the range [0,\sum_k<=b  1/(k-1)/k. The cum_sum_kkp1 array is shifted by 2, hence the b-1
-            randvar = np.random.uniform(0, self.cum_sum_inv_kkp1[b - 1])
-            # determine the maximal k such that the sum is smaller than rand
-            return np.where(self.cum_sum_inv_kkp1[:b] > randvar)[0][0] + 1
-        elif self.alpha == 2:  # Kingman case
-            return 2
-        else:  # other Beta coalescents
-            # calculate the cumulative distribution of the variable part of the merger rate
-            # normalizer and b dependent prefactors omitted
-            cum_rates = np.cumsum(self.gamma_ratiom[2:b + 1] * self.gamma_ratiop[b - 2::-1])
-            randvar = np.random.uniform(0, cum_rates[-1])
-            return np.where(cum_rates > randvar)[0][0] + 2
-
-    def coalesce(self):
-        '''
-        simulates the Beta coalescent process for arbitrary alpha.
-        parameters:
-            K0: the initial population size.
-            alpha: parameter for the Beta coalescent. set to 2 for Kingman and 1 for Bolthausen-Sznitman.
-        '''
-
-        self.init_tree()
-        # while the whole tree is not merged yet
-        while len(self.blocks) != 1:
-            self.coalescence_event()
-
-        self.clean_up_subtree(self.blocks[0])
-        self.BioTree = Phylo.BaseTree.Tree(root=self.blocks[0])
+    print(full_change)
+    print(non_reversion_change)
 
 
 if __name__ == "__main__":
-    alpha = 1.0
-    rate_alpha = 1.5
-    W_dirichlet_alpha = 2.0
-    myGTR = GTR_site_specific.random(3012, alphabet="nuc_nogap", pi_dirichlet_alpha=alpha,
-                                     mu_gamma_alpha=rate_alpha, W_dirichlet_alpha=W_dirichlet_alpha)
-
-    tree_path = "data/BH/intermediate_files/tree_pol_1000.nwk"
-    root_path = "data/BH/intermediate_files/pol_1000_nt_muts.json"
-    root_seq = get_reference_sequence(root_path)
-    root_seq = Seq("".join(root_seq))
-    tree = Phylo.read(tree_path, "newick")
-
-    MySeq = SeqGen(3012, gtr=myGTR, tree=tree)
-    MySeq.evolve(root_seq=root_seq)
-    with open("test.fasta", "wt") as f:
-        AlignIO.write(MySeq.get_aln(), f, "fasta")
+    non_consensus_percentages()
+    # non_consensus_to_non_consensus()
