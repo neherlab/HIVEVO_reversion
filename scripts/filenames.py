6d48271e81dff9d1d65baf7fa644738c7c00e771
Valentin <valentin.druelle@unibas.ch>
2022-01-27 10:59:00 +0100

    path change for run on cluster

--- scripts/filenames.py
+++ scripts/filenames.py
@@ -17,7 +17,8 @@ HIVEVO_ROOT_DATA_PATH = "/scicore/home/neher/druell0000/PhD/MiSeq_HIV_Karolinska
 os.environ["HIVEVO_ROOT_DATA_FOLDER"] = HIVEVO_ROOT_DATA_PATH
 
 # HIV_fitness_landscape fitness_pooled data folder
-HIV_FITNESS_PATH = "/home/valentin/Desktop/richardLab/scicore_local/PhD/HIV_fitness_landscape/data/fitness_pooled"
+# HIV_FITNESS_PATH = "/home/valentin/Desktop/richardLab/scicore_local/PhD/HIV_fitness_landscape/data/fitness_pooled"
+HIV_FITNESS_PATH = "/scicore/home/neher/druell0000/PhD/HIV_fitness_landscape/data/fitness_pooled"
 sys.path.append(HIV_FITNESS_PATH)
 
 
