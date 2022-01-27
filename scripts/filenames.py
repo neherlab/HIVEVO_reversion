16af4848f22a94868b9ffbb24f5f61cf7a0468be
Valentin <valentin.druelle@unibas.ch>
2022-01-27 10:34:59 +0100

    changed file path

--- scripts/filenames.py
+++ scripts/filenames.py
@@ -12,7 +12,7 @@ sys.path.append(HIVEVO_PATH)
 
 # HIVEVO data folder
 # HIVEVO_ROOT_DATA_PATH = "/home/valentin/Desktop/Scicore/GROUP/data/MiSeq_HIV_Karolinska/"
-HIVEVO_ROOT_DATA_PATH = "/scicore/home/neher/GROUP/data/MiSeq_HIV_Karolinska/"
+HIVEVO_ROOT_DATA_PATH = "/scicore/home/neher/druell0000/PhD/MiSeq_HIV_Karolinska/"
 # HIVEVO_ROOT_DATA_PATH = "/home/valentin/Desktop/richardLab/scicore_local/PhD/MiSeq_HIV_Karolinska/"
 os.environ["HIVEVO_ROOT_DATA_FOLDER"] = HIVEVO_ROOT_DATA_PATH
 
