ed3222fa95347a24f9cb587795b4751d57ede4d0
Valentin <valentin.druelle@unibas.ch>
2022-01-27 10:24:16 +0100

    Script to run WH analysis

--- snake_submit.sh
+++ snake_submit.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
-#SBATCH --time=05:59:00
-#SBATCH --qos=6hours
 #SBATCH --output=log/%x.%j.out                 # where to store the output ( %j is the jobID )
 #SBATCH --error=log/%x.%j.err                  # where to store error messages (%x is the jobname)
 
@@ -13,4 +9,4 @@ source $HOME/.bashrc
 # Activate conda env
 conda activate hivevo
 
-echo $@
+{exec_job}
